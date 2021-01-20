352522bf6b2f06c599e25018d43bbfceb5302fb7
eriswan <erikaalves018@gmail.com>
2021-01-19 22:29:35 -0300

    adicionado cards de produtos

--- categoria.php
+++ categoria.php
@@ -31,53 +31,143 @@
     include('menu.html');
     ?>
 
-    <!--Menu Lateral de Categoria-->
-
     <div class="container">
-        <div class="col-md-12">
+        <div class="row">
+            <!--Menu Lateral de Categoria-->
+            <div class="col-md-3 py-4">
+                <div class="menu-categoria">
 
-            <div class="col-md-3 py-3">
-                <div class="box menu-categorias">
-                    <p class="text-center menu-categorias">CATEGORIA</p>
+                    <div class="box menu-categorias">
+                        <p class="text-center menu-categorias">CATEGORIA</p>
+                    </div>
+
+                    <ul class="ul-lista-produtos">
+
+                        <a href="#" class="link-lista-produtos">
+                            <li class="li-lista-produtos">
+                                <i class="icon-produtos fa fa-angle-right fa-lg"></i>
+                                &nbsp;PRODUTO 1
+                            </li>
+                        </a>
+
+                        <a href="#" class="link-lista-produtos">
+                            <li class="li-lista-produtos">
+                                <i class="icon-produtos fa fa-angle-right fa-lg"></i>
+                                &nbsp;PRODUTO 2
+                            </li>
+                        </a>
+
+                        <a href="#" class="link-lista-produtos">
+                            <li class="li-lista-produtos">
+                                <i class="icon-produtos fa fa-angle-right fa-lg"></i>
+                                &nbsp;PRODUTO 3
+                            </li>
+                        </a>
+
+                        <a href="#" class="link-lista-produtos">
+                            <li class="li-lista-produtos">
+                                <i class="icon-produtos fa fa-angle-right fa-lg"></i>
+                                &nbsp;PRODUTO 4
+                            </li>
+                        </a>
+                    </ul>
                 </div>
+            </div>
 
-                <ul class="ul-lista-produtos">
+            <!--Conteudo da Categoria-->
 
-                    <a href="#" class="link-lista-produtos">
-                        <li class="li-lista-produtos">
-                            <i class="icon-produtos fa fa-angle-right fa-lg"></i>
-                            &nbsp;PRODUTO 1
-                        </li>
-                    </a>
+            <div class="col-md-9 py-4">
 
-                    <a href="#" class="link-lista-produtos">
-                        <li class="li-lista-produtos">
-                            <i class="icon-produtos fa fa-angle-right fa-lg"></i>
-                            &nbsp;PRODUTO 2
-                        </li>
-                    </a>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col-lg-4">
+                        <div class="card h-100 text-center" style="width:20rem" ;>
+                            <img src="imagens/sapatilhas/ponta-rosa.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                </p>
+                                <button type="button" class="btn btn-primary">VER MAIS</button>
+                            </div>
+                        </div>
+                    </div>
 
-                    <a href="#" class="link-lista-produtos">
-                        <li class="li-lista-produtos">
-                            <i class="icon-produtos fa fa-angle-right fa-lg"></i>
-                            &nbsp;PRODUTO 3
-                        </li>
-                    </a>
+                    <div class="col-lg-4">
+                        <div class="card h-100 text-center" style="width:20rem" ;>
+                            <img src="imagens/sapatilhas/ponta-marrom.png" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                </p>
+                                <button type="button" class="btn btn-primary">VER MAIS</button>
+                            </div>
+                        </div>
+                    </div>
 
-                    <a href="#" class="link-lista-produtos">
-                        <li class="li-lista-produtos">
-                            <i class="icon-produtos fa fa-angle-right fa-lg"></i>
-                            &nbsp;PRODUTO 4
-                        </li>
-                    </a>
-                </ul>
+                    <div class="col-lg-4">
+                        <div class="card h-100 text-center" style="width:20rem" ;>
+                            <img src="imagens/sapatilhas/ponta-rosa.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                </p>
+                                <button type="button" class="btn btn-primary">VER MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="card h-100 text-center" style="width:20rem" ;>
+                            <img src="imagens/sapatilhas/ponta-marrom.png" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                </p>
+                                <button type="button" class="btn btn-primary">VER MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="card h-100 text-center" style="width:20rem" ;>
+                            <img src="imagens/sapatilhas/ponta-rosa.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                </p>
+                                <button type="button" class="btn btn-primary">VER MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="card h-100 text-center" style="width:20rem" ;>
+                            <img src="imagens/sapatilhas/ponta-marrom.png" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                </p>
+                                <button type="button" class="btn btn-primary">VER MAIS</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-    </div>
     </div>
 
-
+    &nbsp;
 
     <!-- Footer -->
     <?php
