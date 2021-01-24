9766697de4d79ba6c65658ba73aa2e810d610a48
eriswan <erikaalves018@gmail.com>
2021-01-24 18:35:13 -0300

    pasta includes e css consolidado

--- categoria.php
+++ categoria.php
@@ -6,14 +6,14 @@
 
 <!--Head-->
 <?php
-include('head.html');
+include('includes/head.html');
 ?>
 
 <body>
 
     <!--Menu-->
     <?php
-    include('menu.html');
+    include('includes/menu.html');
     ?>
 
     <div class="container">
@@ -156,18 +156,8 @@ include('head.html');
 
     <!-- Footer -->
     <?php
-    include('footer.html');
+    include('includes/footer.html');
     ?>
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-    <!-- MDB -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.0.0/mdb.min.js"></script>
 
 </body>
 
